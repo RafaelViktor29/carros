8da7ae13e72c798bafc7677b646bc309c65c8d01
Rafael Viktor <dev.rafaelviktor@gmail.com>
2024-06-27 07:56:03 -0300

    first commit

---
+++ cars/views.py
@@ -0,0 +1,34 @@
+
+from .models import Car
+from .forms import CarModelForm
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
+
+
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        return cars.filter(model__icontains=search) if search else cars
+
+
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+
+
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+    success_url = '/cars/'
